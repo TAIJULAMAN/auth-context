cd9ac807e7de71ca4c77ab6c2f833bb6765d0890
TAIJULAMAN <taijulaman8@gmail.com>
2023-08-29 23:08:50 +0600

    context api in react with private route

---
+++ src/main.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+
+import Main from './Components/Main';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import AuthProviders from './Components/AuthProviders/AuthProviders';
+import Orders from './Components/Orders'
+import PrivateRoute from './Components/PrivateRoute';
+import Profile from './Components/Profile';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/profile',
+        element:<PrivateRoute><Profile></Profile></PrivateRoute>
+      },
+      {
+        path: '/orders',
+        element:<PrivateRoute><Orders></Orders></PrivateRoute>
+      }
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProviders>
+      <RouterProvider router={router} />
+    </AuthProviders>
+  </React.StrictMode>,
+)
